84f8a4d5e225bc73034f841052affeb411cf0e76
Charlotte Fereday <cfereday@thoughtworks.com>
2017-12-03 14:44:57 +0000

    Updates routes paths for account

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.json({ title: 'Express' });
 });
 
 // GET account name
-router.get('/account-name', function(req, res) {
-  res.json({"bank_id":"nationwide"})
+router.get('/accounts', function(req, res) {
+  res.json({customer_name: "Charlotte Fereday", bank_name:"nationwide", bank_id: "656896"});
+});
+
+// GET account balance
+router.get('/balances', function(req, res) {
+    res.json({balance:"2400"});
+});
+
+// GET account transactions
+router.get('/transactions', function(req, res) {
+    res.json({Pret:"7.60", WHSmith: "10.00", SnappySnaps: "20", Tesco: "56"});
 });
 
 module.exports = router;
